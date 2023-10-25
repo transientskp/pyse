5ef8ec311655f893fc8c87537e74b7f6b2c444db
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2023-10-25 18:00:55 +0200

    A run on an all-sky image from TBB data revealed math overflow problems originating from calculations within fitting.moments_enhanced. An analysis showed that this came from thresholds larger than the peak values. How can this occur? More accurately, why can we have  self.data_bgsubbed[chunk].data[peak_position] <  analysisthresholdmap[peak_position]? Most likely it has to with the subthresholds used for deblending: thresholds[count] = analysisthresholdmap[peak_position] is not correct when deblending is performed. My hunch is that if the subisland constitutes multiple pixels the subthreshold at which the subisland has been found could be lower or higher than analysisthresholdmap[peak_position].

--- sourcefinder/image.py
+++ sourcefinder/image.py
@@ -1087,8 +1087,8 @@ class ImageData(object):
                 measurement = measurements[count]
 
                 peak_position = measurement["xpeak"], measurement["ypeak"]
-                # threshold = measurement["thresh"]
-                thresholds[count] = analysisthresholdmap[peak_position]
+                thresholds[count] = measurement["thresh"]
+                # thresholds[count] = analysisthresholdmap[peak_position]
 
                 local_noise_levels[count] = self.rmsmap[peak_position]
 
