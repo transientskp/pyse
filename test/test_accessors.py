f1c79f9a51bdac52c286f7fd6e4c4d35df9120f8
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2024-10-28 11:24:46 +0100

    Stay within 80 characters per line.

--- test/test_accessors.py
+++ test/test_accessors.py
@@ -9,11 +9,13 @@ from .conftest import DATAPATH
 
 lofar_casatable = os.path.join(
     DATAPATH,
-    "casatable/L55596_000TO009_skymodellsc_wmax6000_noise_mult10_cell40_npix512_wplanes215.img.restored.corr",
+    ("casatable/L55596_000TO009_skymodellsc_wmax6000_noise_mult10_cell40_" +
+     "npix512_wplanes215.img.restored.corr"),
 )
 
 
-@pytest.mark.skipif(not os.path.exists(lofar_casatable), reason="data file not in repo")
+@pytest.mark.skipif(not os.path.exists(lofar_casatable),
+                    reason="data file not in repo")
 def test_no_injection():
     original_ms = accessors.open(lofar_casatable)
     assert_almost_equal(original_ms.tau_time, 58141509)
