be02797c0362c92dc4f726cf68736520b4ae7fbb
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2024-12-07 14:30:19 +0100

    We can now remove the old kappa * sigma clipper
    We can now remove 'sigma_clip', the old kappa * sigma clipper since we have replaced it by a guvectorized version, which is much faster and returns virtually the same background grids, with a relative difference of less than 1e-3.

--- sourcefinder/stats.py
+++ sourcefinder/stats.py
@@ -9,7 +9,6 @@ from scipy.special import erf
 import numba_scipy
 from scipy.optimize import fsolve
 from numba import njit, guvectorize, int32, float32
-from dask.distributed import print as dask_print
 # CODE & NUMBER HANDLING ROUTINES
 
 
@@ -118,71 +117,6 @@ def indep_pixels(n, correlation_lengths):
     corlengthlong, corlengthshort = correlation_lengths
     correlated_area = 0.25 * np.pi * corlengthlong * corlengthshort
     return n / correlated_area
-
-
-def sigma_clip(data, kappa=2.0, max_iter=100,
-               centref=np.median, distf=np.var, my_iterations=0, limit=None):
-    """Iterative clipping
-
-    By default, this performs clipping of the standard deviation about the
-    median of the data. But by tweaking centref/distf, it could be much
-    more general.
-
-    max_iter sets the maximum number of iterations used.
-
-    my_iterations is a counter for recursive operation of the code; leave it
-    alone unless you really want to pretend to jump into the middle of a loop.
-
-    sigma is subtle: if a callable is given, it is passed a copy of the data
-    array and can calculate a clipping limit. See, for e.g., unbiased_sigma()
-    defined above. However, if it isn't callable, sigma is assumed to just set
-    a hard limit.
-
-    To do: Improve documentation
-            -Returns???
-            -How does it make use of the beam? (It estimates the noise correlation)
-    """
-    if my_iterations >= max_iter:
-        # Exceeded maximum number of iterations; return
-        return data, my_iterations
-
-    # Numpy 1.1 breaks std() for MaskedArray: see
-    # <http://www.scipy.org/scipy/numpy/wiki/MaskedArray>.
-    # MaskedArray.compressed() returns a 1-D array of non-masked data.
-    if isinstance(data, MaskedArray):
-        data = data.compressed()
-    centre = centref(data)
-    n = np.size(data)
-    if n < 1:
-        # This chunk is too small for processing; return an empty array.
-        return np.array([]), 0, 0, 0
-
-    clipped_var = distf(data)
-
-    std = np.sqrt(clipped_var)
-
-    if limit is not None:
-        fsolve_std_corr_for_clipping_bias = fsolve(find_true_std, std,
-                                                   args=(limit, std))[0]
-        std_corr_for_clipping_bias, iterations = (
-            newton_raphson_root_finder(f_sigma, std, std,
-                                       limit, 0, limit))
-        dask_print((f"{fsolve_std_corr_for_clipping_bias = :.4e},   "
-                    f"{std_corr_for_clipping_bias = :.4e},   "
-                    f"{std = :.4e},   {limit = :.4e}, {iterations = }\n"))
-    else:
-        std_corr_for_clipping_bias = std
-
-    limit = kappa * std_corr_for_clipping_bias
-
-    newdata = data.compress(abs(data - centre) <= limit)
-
-    if len(newdata) != len(data) and len(newdata) > 0:
-        my_iterations += 1
-        return sigma_clip(newdata, kappa, max_iter, centref, distf,
-                          my_iterations, limit=limit)
-    else:
-        return newdata, std_corr_for_clipping_bias, centre, my_iterations
 
 
 @guvectorize([(float32[:], int32[:], float32[:], float32[:])],
