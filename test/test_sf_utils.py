653bf522994b9516fe7f5e8a559365f7ee5f97ca
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2025-02-09 14:58:51 +0100

    "maximum_pixel_method_variance" test removed
    The "maximum_pixel_method_variance" method has been removed from PySE, so it does not need to be tested any longer.

--- test/test_sf_utils.py
+++ test/test_sf_utils.py
@@ -8,7 +8,7 @@ from sourcefinder.utility.uncertain import Uncertain
 from sourcefinder.utils import circular_mask
 from sourcefinder.utils import generate_result_maps
 from sourcefinder.utils import generate_subthresholds
-from sourcefinder.utils import maximum_pixel_method_variance, fudge_max_pix
+from sourcefinder.utils import fudge_max_pix
 
 
 class TestCircularMask(unittest.TestCase):
@@ -53,27 +53,17 @@ class TestResultMaps(unittest.TestCase):
 
 
 class UtilsTest(unittest.TestCase):
-    # format: semimajor, semiminor, theta, correction, variance
+    # format: semimajor, semiminor, theta, correction
     def setUp(self):
         self.correct_data = (
-            (1.46255645752, 1.30175183614, 1.51619779602, 1.06380431709,
-             0.0017438970306615786),
-            (1.56219151815, 1.25084762573, 1.49080535398, 1.06328376441,
-             0.0017765054643135159),
-            (1.55231285095, 1.24513448079, 1.44660127410, 1.06398549354,
-             0.0018208980607872238)
+            (1.46255645752, 1.30175183614, 1.51619779602, 1.06380431709),
+            (1.56219151815, 1.25084762573, 1.49080535398, 1.06328376441),
+            (1.55231285095, 1.24513448079, 1.44660127410, 1.06398549354)
         )
-
-    def testMaximumPixelMethodVariance(self):
-        for (
-        semimajor, semiminor, theta, correction, variance) in self.correct_data:
-            self.assertEqual(
-                maximum_pixel_method_variance(semimajor, semiminor, theta),
-                variance)
 
     def testFudgeMaxPix(self):
         for (
-        semimajor, semiminor, theta, correction, variance) in self.correct_data:
+        semimajor, semiminor, theta, correction) in self.correct_data:
             self.assertAlmostEqual(fudge_max_pix(semimajor, semiminor, theta),
                                    correction)
 
