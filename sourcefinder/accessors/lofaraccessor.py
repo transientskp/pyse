34b9d46807a7da737770128a3dca24f69b5aba6f
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2025-05-20 18:30:11 +0200

    "numpydoc-style" docstring following GH Copilot

--- sourcefinder/accessors/lofaraccessor.py
+++ sourcefinder/accessors/lofaraccessor.py
@@ -7,14 +7,21 @@ class LofarAccessor():
     Additional metadata required for processing LOFAR images through QC
     checks.
 
-    Attributes:
-        antenna_set (string): Antenna set in use during observation.
-            String; 'LBA_INNER', 'LBA_OUTER', 'LBA_SPARSE', 'LBA' or 'HBA'
-        ncore(int): Number of core stations in use during observation.
-        nremote(int): Number of remote stations in use during observation.
-        nintl(int): Number of international stations in use during observation.
-        subbandwidth(float): Width of a subband in Hz.
-        subbands(int): Number of subbands.
+    Attributes
+    ----------
+    antenna_set : str
+        Antenna set in use during observation. Possible values are
+        'LBA_INNER', 'LBA_OUTER', 'LBA_SPARSE', 'LBA', or 'HBA'.
+    ncore : int
+        Number of core stations in use during observation.
+    nremote : int
+        Number of remote stations in use during observation.
+    nintl : int
+        Number of international stations in use during observation.
+    subbandwidth : float
+        Width of a subband in Hz.
+    subbands : int
+        Number of subbands.
     """
 
     antenna_set: str
