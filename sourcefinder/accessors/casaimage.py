c94de9522d95f97e5631b00ba350d9d005158727
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2025-05-24 14:25:28 +0200

    "numpydoc-style" docstrings for CasaImage class
    Also, as before, some instance methods have been converted to static methods, without changing their intrinsics.

--- sourcefinder/accessors/casaimage.py
+++ sourcefinder/accessors/casaimage.py
@@ -13,11 +13,23 @@ logger = logging.getLogger(__name__)
 
 class CasaImage(DataAccessor):
     """
-    Provides common functionality for pulling data from the CASA image format
+    Provides common functionality for pulling data from the CASA image format.
 
     (Technically known as the 'MeasurementSet' format.)
 
-    NB CasaImage does not provide tau_time or taustart_ts, as there was
+    Parameters
+    ----------
+    url : str
+        The location of the CASA image file.
+    plane : int, default: 0
+        The data plane to extract from the image cube.
+    beam : tuple, default: None
+        Beam parameters (major axis, minor axis, position angle) in degrees.
+        If not provided, these are extracted from the CASA table.
+
+    Notes
+    -----
+    CasaImage does not provide tau_time or taustart_ts, as there was
     no clear standard for these metadata, so cannot be
     instantiated directly - subclass it and extract these attributes
     as appropriate to a given telescope.
@@ -40,15 +52,35 @@ class CasaImage(DataAccessor):
             (bmaj, bmin, bpa) = beam
         else:
             bmaj, bmin, bpa = self.parse_beam(table)
-        self.beam = self.degrees2pixels(
-            bmaj, bmin, bpa, self.pixelsize[0], self.pixelsize[1])
+        self.beam = self.degrees2pixels(bmaj, bmin, bpa, self.pixelsize[0],
+                                        self.pixelsize[1])
 
     def parse_data(self, table, plane=0):
-        """extract and massage data from CASA table"""
+        """
+        Extract and squeeze data from a CASA table, select desired plane and
+        transpose.
+        Squeezing is done to remove any singleton dimensions.
+
+        Parameters
+        ----------
+        table : casacore.tables.table
+            The CASA table from which data is extracted.
+        plane : int, default: 0
+            The data plane to extract from the image cube.
+
+        Returns
+        -------
+        data : numpy.ndarray
+            The extracted and transposed data array. If the data cube has more
+            than two dimensions - after removing singleton dimensions - the
+            plane specified by the `plane` parameter is selected from the first
+            axis.
+        """
         data = table[0]['map'].squeeze()
         planes = len(data.shape)
         if planes != 2:
-            msg = "received datacube with %s planes, assuming Stokes I and taking plane 0" % planes
+            msg = ("received datacube with %s planes, assuming Stokes I "
+                   "and taking plane 0" % planes)
             logger.warning(msg)
             warnings.warn(msg)
             data = data[plane, :, :]
@@ -56,7 +88,21 @@ class CasaImage(DataAccessor):
         return data
 
     def parse_coordinates(self, table):
-        """Returns a WCS object"""
+        """
+        Parse and return a WCS (World Coordinate System) object from the
+        CASA table.
+
+        Parameters
+        ----------
+        table : casacore.tables.table
+            The CASA table from which the WCS information is extracted.
+
+        Returns
+        -------
+        wcs : WCS
+            A WCS object containing the coordinate system information extracted
+            from the CASA table.
+        """
         wcs = WCS()
         my_coordinates = table.getkeyword('coords')['direction0']
         wcs.crval = my_coordinates['crval']
@@ -76,19 +122,67 @@ class CasaImage(DataAccessor):
         return wcs
 
     def parse_frequency(self, table):
-        """extract frequency related information from headers"""
+        """
+        Extract frequency-related information from the CASA table headers.
+
+        Parameters
+        ----------
+        table : casacore.tables.table
+            The CASA table from which frequency information is extracted.
+
+        Returns
+        -------
+        freq_eff : float
+            The effective frequency (rest frequency) in Hz.
+        freq_bw : float
+            The frequency bandwidth (channel width) in Hz.
+        """
         freq_eff = table.getkeywords()['coords']['spectral2']['restfreq']
         freq_bw = table.getkeywords()['coords']['spectral2']['wcs']['cdelt']
         return freq_eff, freq_bw
 
     def parse_beam(self, table):
         """
-        Returns:
-          - Beam parameters, (semimajor, semiminor, parallactic angle) in
-            (pixels,pixels, radians).
+        Extract beam parameters from the CASA table.
+
+        Parameters
+        ----------
+        table : casacore.tables.table
+            The CASA table from which the beam parameters are extracted.
+
+        Returns
+        -------
+        tuple
+            A tuple containing:
+            - semimajor : float
+                The semi-major axis of the beam in degrees.
+            - semiminor : float
+                The semi-minor axis of the beam in degrees.
+            - synthesized beam position angle : float
+                The angle of the major axis of the synthesized beam,
+                measured east from local north, in degrees.
         """
 
         def ensure_degrees(quantity):
+            """
+            Convert a quantity to degrees.
+
+            Parameters
+            ----------
+            quantity : dict
+                A dictionary containing the value and unit of the quantity to
+                be converted. Supported units are 'deg', 'arcsec', and 'rad'.
+
+            Returns
+            -------
+            float
+                The value of the quantity converted to degrees.
+
+            Raises
+            ------
+            Exception
+                If the unit of the quantity is not recognized.
+            """
             if quantity['unit'] == 'deg':
                 return quantity['value']
             elif quantity['unit'] == 'arcsec':
@@ -104,26 +198,45 @@ class CasaImage(DataAccessor):
         bpa = ensure_degrees(restoringbeam['positionangle'])
         return bmaj, bmin, bpa
 
-    def parse_phase_centre(self, table):
-        # The units for the pointing centre are not given in either the image
-        # cubeitself or in the ICD. Assume radians.
-        # Note that we'll return the RA modulo 360 so it's always 0 <= RA < 360
+    @staticmethod
+    def parse_phase_centre(table):
+        """
+        Assume the units for the pointing centre are in radians and return the
+        RA modulo 360.
+
+        Parameters
+        ----------
+        table : casacore.tables.table
+            The CASA table from which the pointing centre is extracted.
+
+        Notes
+        -----
+        The units for the pointing centre are not given in either the image
+        cube itself nor in the image content description. Assume radians. The
+        Right Ascension (RA) is returned modulo 360 to ensure it is always
+        within the range 0 <= RA < 360.
+        """
         centre_ra, centre_decl = table.getkeyword('coords')['pointingcenter'][
             'value']
         return degrees(centre_ra) % 360, degrees(centre_decl)
 
-    def parse_taustartts(self, table):
+    @staticmethod
+    def parse_taustartts(table):
         """
         Extract integration start-time from CASA table header.
 
         This applies to some CASA images (typically those created from uvFITS
         files) but not all, and so should be called for each sub-class.
 
-        Arguments:
-            table: MAIN table of CASA image.
+        Parameters
+        ----------
+        table : casacore.tables.table
+            The MAIN table of the CASA image.
 
-        Returns:
-            Time of image start as a instance of ``datetime.datetime``
+        Returns
+        -------
+        datetime.datetime
+            Time of observation start as an instance of `datetime.datetime`.
         """
         obsdate = table.getkeyword('coords')['obsdate']['m0']['value']
         return mjd2datetime(obsdate)
@@ -133,12 +246,17 @@ class CasaImage(DataAccessor):
         """
         Find all the unique values in a particular column of a CASA table.
 
-        Arguments:
-          - table:       ``casacore.tables.table``
-          - column_name: ``str``
+        Parameters
+        ----------
+        table : casacore.tables.table
+            The CASA table from which the column values are extracted.
+        column_name : str
+            The name of the column to extract unique values from.
 
-        Returns:
-          - ``numpy.ndarray`` containing unique values in column.
+        Returns
+        -------
+        numpy.ndarray
+            An array containing the unique values in the specified column.
         """
         return table.query(
             columns=column_name, sortlist="unique %s" % (column_name)
