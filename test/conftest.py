14a36416ffbf85ee4fba2fad6f39c012eb784c1b
Timo Millenaar <timo.m@live.nl>
2025-06-06 16:28:54 +0200

    Fix warnings regarding pytest-cov and multiprocessing spawn method

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,12 @@
 import os
 from pathlib import Path
 import warnings
+import multiprocessing
 
 DEFAULT = str((Path(__file__).parent / "data").absolute())
 DATAPATH = os.environ.get("TKP_TESTPATH", DEFAULT)
+
+multiprocessing.set_start_method("spawn", force=True)
 
 if not os.access(DATAPATH, os.X_OK):
     warnings.warn(f"can't access {DATAPATH}")
