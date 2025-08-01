afb38f5af529081cd9743589619ccd3e731e4e91
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2025-08-01 16:42:48 +0200

    API docs using autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,55 +1,50 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sourcefinder
 
-# -- Path setup --------------------------------------------------------------
+project = "PySE"
+authors = [
+    "Hanno Spreeuw",
+    "John Swinbank",
+    "Gijs Molenaar",
+    "Tim Staley",
+    "Evert Rol",
+    "John Sanders",
+    "Bart Scheers",
+    "Mark Kuiack",
+    "Suvayu Ali",
+    "Timo Millenaar",
+    "Antonia Rowlinson",
+]
+author = ", ".join(authors)
+copyright = ", ".join(["2024", *authors])
+release = sourcefinder.__version__
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
-
-
-# -- Project information -----------------------------------------------------
-
-project = 'PySE'
-copyright = '2024, Hanno Spreeuw, John Swinbank, Gijs Molenaar, Tim Staley, Evert Rol, John Sanders, Bart Scheers, Mark Kuiack, Suvayu Ali, Timo Millenaar, Antonia Rowlinson'
-author = 'Hanno Spreeuw, John Swinbank, Gijs Molenaar, Tim Staley, Evert Rol, John Sanders, Bart Scheers, Mark Kuiack, Suvayu Ali, Timo Millenaar, Antonia Rowlinson'
-
-# The full version, including alpha/beta/rc tags
-release = '0.3.2'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autodoc.typehints",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.extlinks",
+    "numpydoc",
+    "autoapi.extension",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# autoapi_keep_files = True
+autoapi_dirs = ["../../sourcefinder"]
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "private-members",
+    "show-inheritance",
+    "show-module-summary",
+    "imported-members",
+]
+autoapi_member_order = "groupwise"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+templates_path = ["_templates"]
+
+# source file discovery
 exclude_patterns = []
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# HTML options
+html_theme = "furo"
